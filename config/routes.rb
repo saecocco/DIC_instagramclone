f938dfef99b52590363d1da345608ebae4bb8f65
saecocco <saekoy16@gmail.com>
2021-05-17 13:21:18 +0000

    ユーザー登録フォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
