810e15b5a61f137413e5b01ac6c3d83a0ea7836a
saecocco <saekoy16@gmail.com>
2021-05-17 13:38:44 +0000

    ログイン状態での処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authentivate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
     else
       flash.now[:danger] = 'Failed login'
       render :new
