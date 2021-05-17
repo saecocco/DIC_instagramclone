3a6c414f7c55a01fe3398384c4a49a8d96f0312c
saecocco <saekoy16@gmail.com>
2021-05-17 13:28:27 +0000

    ユーザ詳細ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
