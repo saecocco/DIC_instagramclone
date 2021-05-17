e6087fee443c3b97a3628fad57e165df9afcc033
saecocco <saekoy16@gmail.com>
2021-05-17 12:52:06 +0000

    投稿確認画面を実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -56,6 +56,10 @@ class PicturesController < ApplicationController
     end
   end
 
+  def confirm
+    @picture = Picture.new(picture_params)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_picture
