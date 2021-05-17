fa777f276a91d4d046f281aac50404ec27767e6d
saecocco <saekoy16@gmail.com>
2021-05-17 13:35:29 +0000

    セッションの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authentivate(params[:session][:password])
+    else
+      flash.now[:danger] = 'Failed login'
+      render :new
+    end
+  end
 end
