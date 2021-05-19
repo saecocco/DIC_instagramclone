f20284397c17d59442942f642b4257ae54abdf24
saecocco <saekoy16@gmail.com>
2021-05-19 12:35:33 +0000

    ユーザのログアウト機能を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,22 @@
 class SessionsController < ApplicationController
-    skip_before_action :login_required, only [:new,:create]
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authentivate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = 'Failed login'
+      flash[:danger] = 'Failed login'
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
+    flash[:notice] = 'Logout'
+    redirect_to new_session_path
+  end
 end
