ffee5a35598b45aef3c70a90934ce5bf623a2e67
saecocco <saekoy16@gmail.com>
2021-07-06 13:12:45 +0000

    メール送信機能を実装中

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -35,7 +35,7 @@ class PicturesController < ApplicationController
     respond_to do |format|
       if @picture.save
         PictureMailer.picture_mail(@picture).deliver
-        format.html { redirect_to @picture, notice: "Picture was successfully created." }
+        format.html { redirect_to pictures_path, notice: "Picture was successfully created." }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new, status: :unprocessable_entity }
